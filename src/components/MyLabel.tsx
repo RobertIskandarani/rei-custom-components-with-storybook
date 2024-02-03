6ada8cefeca372a401d47e992b0def0ca7b9541e
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-02-03 11:45:04 -0300

    Storybook listo

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,44 @@
+import './MyLabel.css';
+
+interface Props {
+  /**
+   * Text to display
+   */
+  label: string;
+
+  /**
+   * Label size
+   */
+  size?: 'normal' | 'h1' | 'h2' | 'h3';
+
+  /**
+   * Capitalize all letters
+   */
+  allCaps?: boolean;
+
+  /**
+   * Label color
+   */
+  color?: 'text-primary' | 'text-secondary' | 'text-tertiary';
+
+  /**
+   * Font color
+   */
+  fontColor?: string;
+}
+
+export function MyLabel({
+  label,
+  size = 'normal',
+  allCaps = false,
+  color,
+  fontColor,
+}: Props) {
+  return (
+    <span style={{ color: fontColor }} className={`${size} ${color} label`}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+}
+
+export default MyLabel;
